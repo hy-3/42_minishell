51f5b9843e0df8a5f04d8958a31ad03d6a48598d
Hiroki Yamamoto <hymt73@gmail.com>
2022-10-04 17:18:50 +0200

    fixed ft_strtrim

--- lib/libft/ft_strtrim.c
+++ lib/libft/ft_strtrim.c
@@ -5,99 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hiyamamo <hiyamamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/13 17:39:50 by hiyamamo          #+#    #+#             */
-/*   Updated: 2022/04/16 13:43:53 by hiyamamo         ###   ########.fr       */
+/*   Created: 2022/01/25 18:47:38 by enevado           #+#    #+#             */
+/*   Updated: 2022/10/04 17:16:14 by hiyamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	find_index_of_trimmed_start(char const *s1, char const *set)
-{
-	int	res;
-	int	i;
-	int	k;
-	int	flag;
-
-	res = 0;
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		k = 0;
-		flag = 0;
-		while (set[k] != '\0')
-		{
-			if (s1[i] == set[k])
-				flag = 1;
-			k++;
-		}
-		if (flag == 0)
-			break ;
-		i++;
-	}
-	if (s1[i] == '\0')
-		i = 0;
-	return (res + i);
-}
-
-int	find_index_of_trimmed_end(char const *s1, char const *set)
-{
-	int	res;
-	int	i;
-	int	k;
-	int	flag;
-
-	res = 0;
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	i--;
-	while (i > 0)
-	{
-		k = 0;
-		flag = 0;
-		while (set[k] != '\0')
-		{
-			if (s1[i] == set[k])
-				flag = 1;
-			k++;
-		}
-		if (flag == 0)
-			break ;
-		i--;
-	}
-	return (res + i);
-}
-
-/**
- * @brief Trim out [set] from begging and end of [s1].
- *
- * @param s1(char const *): Source string to be trimmed.
- * @param set(char const *): A set of char which the function will trim.
- * @return (char *): Trimmed string.
- */
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		index_of_start;
-	int		index_of_end;
-	char	*res;
+	size_t	s1len;
 
-	if (s1 == NULL || set == NULL)
+	if (!s1)
 		return (NULL);
-	index_of_start = find_index_of_trimmed_start(s1, set);
-	index_of_end = find_index_of_trimmed_end(s1, set);
-	res = malloc((index_of_end - index_of_start + 2) * sizeof(char));
-	if (res == NULL)
-		return (NULL);
-	if (index_of_start == 0 && index_of_end == 0)
-	{
-		res[0] = '\0';
-		return (res);
-	}
-	i = 0;
-	while ((index_of_end - index_of_start + 1) > 0)
-		res[i++] = s1[index_of_start++];
-	res[i] = '\0';
-	return (res);
+	while (ft_strchr(set, *s1) && *s1 != '\0')
+		s1++;
+	s1len = ft_strlen(s1);
+	if (!*s1)
+		return (ft_strdup(""));
+	while (ft_strchr(set, s1[s1len]) && s1len > 0)
+		s1len--;
+	return (ft_substr(s1, 0, s1len + 1));
 }
